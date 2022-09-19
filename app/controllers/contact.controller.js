1289fe23b2b13000e9cdaee37ecd4480794bc652
yenb1910487 <yenb1910487@student.ctu.edu.vn>
2022-09-19 21:26:55 +0700

    vscode

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,18 @@
+exports.create = (req, res) => {
+    res.send({message: "create handler"});
+};
+exports.findAll = (req, res) => {
+    res.send({message: "findAll handler"});
+};
+exports.findOne = (req, res) => {
+    res.send({message: "finOne handler"});
+};
+exports.update = (req, res) => {
+    res.send({message: "update handler"});
+};
+exports.delete = (req, res) => {
+    res.send({message: "delete handler"});
+};
+exports.findAllFavorite = (req, res) => {
+    res.send({message: "findAllFavorite handler"});
+};
